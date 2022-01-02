e93b13ba514a1c5a7a6daf41b41758dbb1a9da0f
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-02 21:26:08 +0300

    add user interaction (react)

--- client/src/UserSignupPage.jsx
+++ client/src/UserSignupPage.jsx
@@ -1,29 +1,55 @@
 import React from 'react';
 
 //stateful component
-class UserSignupPage extends React.Component{
+class UserSignupPage extends React.Component {
+
+    state = {
+        username: null,
+        displayName: null,
+        password: null,
+        passwordRepeat: null,
+    }
+
+    onChange = event => {
+        const {name, value} = event.target;
+        this.setState({
+            [name]: value
+        })
+    }
+
     //overrided render method
-    render(){
-        return(
+    render() {
+        return (
             <form>
                 <h1>
                     Sign up
                 </h1>
                 <div>
                     <label>Username</label>
-                    <input type="text"/>
+                    <input type="text"
+                           name="username"
+                           onChange={this.onChange}
+                    />
                 </div>
                 <div>
                     <label>Display Name</label>
-                    <input type="text"/>
+                    <input type="text"
+                           name="displayName"
+                           onChange={this.onChange}/>
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password"/>
+                    <input type="password"
+                           name="password"
+                           onChange={this.onChange}
+                    />
                 </div>
                 <div>
                     <label>Password Repeat</label>
-                    <input type="password"/>
+                    <input type="password"
+                           name="passwordRepeat"
+                           onChange={this.onChange}
+                    />
                 </div>
                 <button>Sign Up</button>
             </form>
