3347def1c41316998e45d7666a295cd96ad9dc05
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-02 23:52:46 +0300

    add spring security password encoded

--- ws/src/main/java/com/hoaxify/ws/user/UserService.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserService.java
@@ -1,21 +1,29 @@
 package com.hoaxify.ws.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 	
 	UserRepository userRepository;
+	PasswordEncoder passwordEncoder;
 	
 	//constructor injection
 	//@Autowired - one constructor/non required
 	public UserService(UserRepository userRepository) {
 		super();
 		this.userRepository = userRepository;
+		this.passwordEncoder = new BCryptPasswordEncoder();
 	}
 
 	public void save(User user) {
+		//password encrypt
+		String encryptedPassword = this.passwordEncoder.encode(user.getPassword());
+		user.setPassword(encryptedPassword);
+		
 		userRepository.save(user);
 	}
 
