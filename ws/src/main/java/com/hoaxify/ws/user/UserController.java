e7d23b647544a21269976efcc19278f382daa422
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-02 23:08:04 +0300

    add db record, ddl-auto userRepository

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -2,6 +2,7 @@ package com.hoaxify.ws.user;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,11 +11,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 	
-	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	//private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	
+	//dependency injection
+	@Autowired
+	UserRepository userRepository;
 
 	@CrossOrigin
 	@PostMapping("/api/1.0/users")
 	public void createUser(@RequestBody User user) {
-		log.info(user.toString());
+		//log.info(user.toString());
+		userRepository.save(user);
 	}
+	
 }
