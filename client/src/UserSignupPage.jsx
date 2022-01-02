b8b6beb50083e2c18c48ef03281e30a0d2cb9644
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-02 19:38:21 +0300

    add usersignuppage (react)

---
+++ client/src/UserSignupPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+//stateful component
+class UserSignupPage extends React.Component{
+    //overrided render method
+    render(){
+        return(
+            <h1>
+                Sign up
+            </h1>
+        );
+    };
+}
+
+export default UserSignupPage;
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import UserSignupPage from "./UserSignupPage";
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <UserSignupPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
