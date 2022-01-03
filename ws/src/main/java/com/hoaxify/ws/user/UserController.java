7d43e72c29e1b5e491fc1588cb56db4ec297b900
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-04 01:49:03 +0300

    add ApiError class, impleneted valid error..

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,5 +1,8 @@
 package com.hoaxify.ws.user;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericResponse;
 
 @RestController
@@ -26,7 +30,13 @@ public class UserController {
 	public ResponseEntity<?> createUser(@RequestBody User user) {
 		String username = user.getUsername();
 		if(username == null || username.isEmpty()) {
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+			ApiError error = new ApiError(400, "Validation error", "/api/1.0/users");
+			
+			Map<String, String> validationErrors = new HashMap<>();
+			validationErrors.put("username","Username cannot be null");
+			error.setValidationErrors(validationErrors);
+			
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 		}
 		userService.save(user);
 		return ResponseEntity.ok(new GenericResponse("User Created!"));
