8d54d4eb69aa8ba2d5cb7544eb17ff0e0c2b5ea2
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-09 02:17:40 +0300

    add UniqueUsername spring

---
+++ ws/src/main/java/com/hoaxify/ws/user/UniqueUsername.java
@@ -0,0 +1,29 @@
+package com.hoaxify.ws.user;
+
+import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
+import static java.lang.annotation.ElementType.CONSTRUCTOR;
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Repeatable;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import javax.validation.constraints.NotNull.List;
+
+@Target({ FIELD })
+@Retention(RUNTIME)
+@Constraint(validatedBy = {UniqueUsernameValidator.class})
+public @interface UniqueUsername {
+	String message() default "Username must be unique";
+
+	Class<?>[] groups() default { };
+
+	Class<? extends Payload>[] payload() default { };
+
+}
--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -21,7 +21,7 @@ public class User {
 	
 	@NotNull
 	@Size(min = 4, max=255)
-	@Column(unique = true)
+	@UniqueUsername
 	private String username;
 	
 	@NotNull
