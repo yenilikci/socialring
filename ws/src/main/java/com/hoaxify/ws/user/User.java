f35a3b70241ed46adb656710c42cdfcf3b8565e4
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-08 04:12:31 +0300

    add column unique annotation

--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -1,5 +1,6 @@
 package com.hoaxify.ws.user;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -20,6 +21,7 @@ public class User {
 	
 	@NotNull
 	@Size(min = 4, max=255)
+	@Column(unique = true)
 	private String username;
 	
 	@NotNull
