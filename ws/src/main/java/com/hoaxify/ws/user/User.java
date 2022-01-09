584a85748282ef03189d01bb031bb4c925f1b855
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-09 16:51:01 +0300

    add login success res

--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -9,6 +9,10 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonView;
+import com.hoaxify.ws.shared.Views;
+
 import lombok.Data;
 
 @Data
@@ -23,16 +27,22 @@ public class User {
 	@NotNull(message = "{hoaxify.constraint.username.NotNull.message}")
 	@Size(min = 4, max=255)
 	@UniqueUsername
+	@JsonView(Views.Base.class)
 	private String username;
 	
 	@NotNull
 	@Size(min = 4, max=255)
+	@JsonView(Views.Base.class)
 	private String displayName;
 	
 	@NotNull
 	@Size(min = 8, max=255)
 //	@Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$") 
 	@Pattern(regexp="^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "{hoaxify.constraint.password.Pattern.message}") 
+//	@JsonIgnore
+	@JsonView(Views.Sensitive.class)
 	private String password;
 	
+	@JsonView(Views.Base.class)
+	private String image;
 }
