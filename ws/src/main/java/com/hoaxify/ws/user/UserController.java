3555309a042e56fd4c797b14296a25a4076cd033
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-09 21:46:48 +0300

    modified errorhandler , err message common solution

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -37,16 +37,16 @@ public class UserController {
 		return new GenericResponse("User Created!");
 	}
 	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ApiError handleValidationException(MethodArgumentNotValidException exception) {
-		ApiError error = new ApiError(400, "Validation error", "/api/1.0/users");
-		Map<String, String> validationErrors = new HashMap<>();
-		for(FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
-			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
-		}
-		error.setValidationErrors(validationErrors);
-		return error;
-	}
+//	@ExceptionHandler(MethodArgumentNotValidException.class)
+//	@ResponseStatus(HttpStatus.BAD_REQUEST)
+//	public ApiError handleValidationException(MethodArgumentNotValidException exception) {
+//		ApiError error = new ApiError(400, "Validation error", "/api/1.0/users");
+//		Map<String, String> validationErrors = new HashMap<>();
+//		for(FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+//			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+//		}
+//		error.setValidationErrors(validationErrors);
+//		return error;
+//	}
 	
 }
