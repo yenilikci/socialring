6fb41aa78a6e351fac21afc85c18e4b39dcbd2af
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-09 22:27:22 +0300

    login page show error messages

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -6,37 +6,56 @@ import {login} from '../api/apiCalls';
 class LoginPage extends Component {
     state = {
         username: null,
-        password: null
+        password: null,
+        error: null
     }
 
     onChange = event => {
         const {name, value} = event.target;
         this.setState({
-            [name]: value
+            [name]: value,
+            error: null
         })
     };
 
-    onClickLogin = event => {
+    onClickLogin = async event => {
         event.preventDefault();
         const {username, password} = this.state;
         const creds = {
             username,
             password
+        };
+        this.setState({
+            error: null
+        });
+        try {
+            await login(creds);
+        } catch (apiError) {
+            this.setState({
+                error: apiError.response.data.message
+            });
         }
-        login(creds);
     };
 
     render() {
         const {t} = this.props;
+        const {username,password,error} = this.state;
+        const buttonEnabled = username && password;
+
         return (
             <div className="container">
                 <form action="">
                     <h1 className="text-center">{t('Login')}</h1>
                     <Input label={t('Username')} name="username" onChange={this.onChange}/>
                     <Input label={t('Password')} name="password" type="password" onChange={this.onChange}/>
+                    {error &&
+                    <div className="alert alert-danger mt-2">
+                        {error}
+                    </div>}
                     <div className="text-center">
                         <button className="btn btn-primary mt-4"
-                        onClick={this.onClickLogin}
+                                onClick={this.onClickLogin}
+                                disabled={!buttonEnabled}
                         >{t('Login')}</button>
                     </div>
                 </form>
