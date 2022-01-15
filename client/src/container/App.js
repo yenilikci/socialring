178c422eb0df2b868ffbd49af7f86e1d2ea1693a
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-15 17:06:50 +0300

    add router

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -4,11 +4,21 @@ import LoginPage from "../pages/LoginPage";
 import LanguageSelector from "../components/LanguageSelector";
 import UserSignupPage from "../pages/UserSignupPage";
 import HomePage from "../pages/HomePage";
+import {HashRouter, Route, Redirect, Switch} from "react-router-dom";
+import UserPage from "../pages/UserPage";
 
 function App() {
     return (
-        <div className="row">
-            <HomePage/>
+        <div>
+            <HashRouter>
+               <Switch>
+                   <Route path="/" exact component={HomePage}/>
+                   <Route path="/login" component={LoginPage}/>
+                   <Route path="/signup" component={UserSignupPage}/>
+                   <Route path="/user/:username" component={UserPage}/>
+                   <Redirect to="/"/>
+               </Switch>
+            </HashRouter>
             <LanguageSelector/>
         </div>
     );
