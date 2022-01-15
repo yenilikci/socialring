8a4387790381c7d44427dd3783489acddfce53f1
yenilikci <57464067+yenilikci@users.noreply.github.com>
2022-01-15 23:49:51 +0300

    user login status top bar change

--- client/src/components/TopBar.js
+++ client/src/components/TopBar.js
@@ -1,31 +1,56 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from '../assets/social.png';
-import { Link } from 'react-router-dom';
-import { withTranslation } from 'react-i18next';
+import {Link} from 'react-router-dom';
+import {withTranslation} from 'react-i18next';
 
 class TopBar extends Component {
+
+    state = {
+        isLoggedIn: true,
+        username: 'user1'
+    };
+
     render() {
-        const { t } = this.props;
+        const {t} = this.props;
+        const {isLoggedIn, username} = this.state;
+
+        let links = (
+            <ul className="navbar-nav ml-auto">
+                <li>
+                    <Link className="nav-link" to="/login">
+                        {t('Login')}
+                    </Link>
+                </li>
+                <li>
+                    <Link className="nav-link" to="/signup">
+                        {t('Sign up')}
+                    </Link>
+                </li>
+            </ul>
+        );
+        if(isLoggedIn) {
+            links = (
+                <ul className="navbar-nav ml-auto">
+                    <li>
+                        <Link className="nav-link" to={`/user/${username}`}>
+                            {username}
+                        </Link>
+                    </li>
+                    <li className="nav-link">
+                        {t('Logout')}
+                    </li>
+                </ul>
+            )
+        }
+
         return (
             <div className="shadow-sm bg-light mb-2">
                 <nav className="navbar navbar-light container navbar-expand">
                     <Link className="navbar-brand" to="/">
-                        <img src={logo} width="60" alt="Hoaxify Logo" />
+                        <img src={logo} width="60" alt="Hoaxify Logo"/>
                         Hoaxify
                     </Link>
-
-                    <ul className="navbar-nav ml-auto">
-                        <li>
-                            <Link className="nav-link" to="/login">
-                                {t('Login')}
-                            </Link>
-                        </li>
-                        <li>
-                            <Link className="nav-link" to="/signup">
-                                {t('Sign up')}
-                            </Link>
-                        </li>
-                    </ul>
+                    {links}
                 </nav>
             </div>
         );
